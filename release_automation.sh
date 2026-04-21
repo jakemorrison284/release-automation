499a2745df0f1ed95691917b12a68199fab81ed6
jakemorrison284 <jakemorrison@walla.co.il>
2026-04-21 15:31:09 +0300

    Add release_automation.sh script to enhance the release process

---
+++ release_automation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script to automate the release process
+
+# Define variables
+VERSION="$(date +%Y%m%d%H%M%S)"
+
+# Build the release
+echo "Building release version $VERSION..."
+
+# Additional release steps can be added here
+
+echo "Release $VERSION completed successfully!"
